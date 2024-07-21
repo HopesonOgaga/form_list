41a57928e0fbb7da222eea60419231b575572635
hopeson <hopesonogaga@gmail.com>
2024-07-21 09:13:20 +0100

    list complete

--- src/App.js
+++ src/App.js
@@ -17,12 +17,18 @@ export default function App() {
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
-
+  function handleClearList() {
+    setItems([]);
+  }
   return (
     <div>
       <Logo />
       <Form onAddItems={handleItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onClearList={handleClearList}
+      />
       <Stats />
     </div>
   );
@@ -74,14 +80,45 @@ function Form({ onAddItems }) {
   );
 }
 // renders props
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem , onClearList}) {
+  const [sort, setSortBy] = useState("input");
+  const [clear, setClear] = useState("");
+
+  let sortedItems;
+
+  if (sort === "input") sortedItems = items;
+
+  if (sort === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sort === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul className="">
-        {items.map((items) => (
+        {sortedItems.map((items) => (
           <Item items={items} onDeleteItem={onDeleteItem} key={items.id} />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          value={sort}
+          onChange={(e) => {
+            setSortBy(e.target.value);
+          }}
+        >
+          <option value="input">sort by input order </option>
+          <option value="description">sort by description</option>
+          <option value="packed ">sort by packed status</option>
+        </select>
+        <button className="" onClick={onClearList}>
+          clear list
+        </button>
+      </div>
     </div>
   );
 }
@@ -89,6 +126,7 @@ function PackingList({ items, onDeleteItem }) {
 function Item({ items, onDeleteItem }) {
   return (
     <li>
+      <input type="checkbox" value={items.packed} onChange={() => {}}></input>
       <span
         className=""
         style={items.packed ? { textDecoration: "line-through" } : {}}
